d5416afb74e7f2766eac8464436d1b55b32ec44e
kaganozupek <kgn.ozupek26@gmail.com>
2014-01-10 21:13:35 +0200

    qwe

--- SnitchV5/src/com/khora/snitch/FragmentMainLogin.java
+++ SnitchV5/src/com/khora/snitch/FragmentMainLogin.java
@@ -10,7 +10,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 public class FragmentMainLogin extends Fragment {
-//PUSHHHH
+
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
